92a8c157f40c2f28de02ddf5aeddfc10035db734
Olivia <oliviafedato@gmail.com>
2025-02-05 21:41:20 -0700

    updated import file paths

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,13 @@
 import './App.css'
 import {HashRouter as Router, Routes, Route} from "react-router-dom"
-import { About } from '../pages/About'
-import { CreateResource } from '../pages/CreateResource'
-import { Contact } from '../pages/Contact'
-import { Home } from '../pages/Home'
-import { Landing } from '../pages/Landing'
-import { ResourceIndex } from '../pages/ResourceIndex'
-import { ViewResource } from '../pages/ViewResource'
-import { Layout } from '../components/Layout'
+import { About } from './pages/About'
+import { CreateResource } from './pages/CreateResource'
+import { Contact } from './pages/Contact'
+import { Home } from './pages/Home'
+import { Landing } from './pages/Landing'
+import { ResourceIndex } from './pages/ResourceIndex'
+import { ViewResource } from './pages/ViewResource'
+import { Layout } from './components/Layout'
 
 function App() {
 
